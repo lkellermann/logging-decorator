322bb20fb1b2f76ddaaf9ae7d338abde836a6219
Leandro Kellermann de Oliveira <lk.kellermann@hotmail.com>
2021-11-07 14:43:29 -0300

    fix: included _create_client_summary interface (dev).

--- src/LoggingDecorator/LoggingDecorator.py
+++ src/LoggingDecorator/LoggingDecorator.py
@@ -2,7 +2,15 @@
 Main module to serve logd decorator.
 """
 # pylint: disable=C0103
-from .wrapped.logging import WrappedLoggingClass
+from .wrapped.custom_logging import WrappedLoggingClass
+from .wrapped import summary
+
+def _create_client_summary(wrapped_object):
+    streams = [summary.LoggerStreamSummary]
+    for stream in streams:
+        obj = stream(wrapped_object)
+        obj.create_summary()
+    return True
 
 def logd(*dargs, **dkwargs) -> object:
     """
@@ -16,15 +24,19 @@ def logd(*dargs, **dkwargs) -> object:
 
         def wrapper(original_method):
             def wrapped_function(*args, **kwargs):
-                return WrappedLoggingClass()\
+                wrapped_object = WrappedLoggingClass()
+                _create_client_summary(wrapped_object)
+                return wrapped_object\
                     .call(original_method, *args, **kwargs)
             return wrapped_function
         return wrapper(dargs[0])
 
 
-    def wrapper(original_method):   # pylint: disable=E0102
+    def wrapper(original_method):# pylint: disable=E0102
         def wrapped_function(*args, **kwargs):
-            return WrappedLoggingClass(*dargs, **dkwargs)\
+            wrapped_object = WrappedLoggingClass(*dargs, **dkwargs)
+            _create_client_summary(wrapped_object)
+            return wrapped_object\
                 .call(original_method, *args, **kwargs)
         return wrapped_function
     return wrapper
