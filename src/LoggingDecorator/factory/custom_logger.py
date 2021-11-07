673f1888d85fbc211467f070497a6648b43d4e37
Leandro Kellermann de Oliveira <lk.kellermann@hotmail.com>
2021-11-07 18:37:35 -0300

    fix: included missing setLevel (dev).

--- src/LoggingDecorator/factory/custom_logger.py
+++ src/LoggingDecorator/factory/custom_logger.py
@@ -50,6 +50,7 @@ class CustomLogger(AbstractCustomLogger):       # pylint: disable=R0903
         cwd_name =  self.directory_parameters.cwd_name
         logger = self.logging.getLogger(cwd_name)
         logger.handlers.clear()
+        logger.setLevel(self.wrapped.logging_level)
         logger.addHandler(handler_object)
         self._logger = logger
         return self._logger
@@ -68,19 +69,18 @@ class CustomLogger(AbstractCustomLogger):       # pylint: disable=R0903
         # print(f'{help(self.logger)}')
         # print(f'{self.logger.handle}')
         if self.logger.hasHandlers:
-            # TODO get method attributes to log here:
-            # print(f'PRINT - Working directory:     {self.directory_parameters.cwd}')
-            self.logger.warning(f'Working directory:     {self.directory_parameters.cwd}')
-            # self.logger.warning(f'Module name:           {self.wrapped.module_name}')
-            # self.logger.warning(f'Method name:           {self.wrapped.method_name}')
-            # self.logger.warning(f'Error message:         {self.wrapped.error_message}')
-            # self.logger.warning(f'Start time:            {self.wrapped.start_time}')
-            self.logger.warning(f'End time:              {end_time}')
-            self.logger.warning(f'Elapsed time:          {elapsed_time}')
-            # self.logger.debug(f'Variables:             {self.wrapped.method_variables}')
+            self.logger.info(f'Working directory:     {self.directory_parameters.cwd}')
+            self.logger.info(f'Module name:           {self.wrapped.module_name}')
+            self.logger.info(f'Method name:           {self.wrapped.method_name}')
+            self.logger.debug(f'Error message:         {self.wrapped.error_message}')
+            self.logger.info(f'Start time:            {self.wrapped.start_time}')
+            self.logger.info(f'End time:              {end_time}')
+            self.logger.info(f'Elapsed time:          {elapsed_time}')
+            self.logger.info(f'Variables:             {self.wrapped.method_variables}')
+            self.logger.critical('teste')
         else:
             raise ValueError('Logger has no handler attached.')
         if self.wrapped.file:
-            self.logger.debug(f'Log path:              {self.wrapped.output_log}')
+            self.logger.info(f'Log path:              {self.wrapped.output_log}')
 
         self.logging.shutdown()
