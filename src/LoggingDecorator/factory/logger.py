35fa4ba04997be019f87b439dc48000aaca6824a
Leandro Kellermann de Oliveira <lk.kellermann@hotmail.com>
2021-11-07 14:46:50 -0300

    fix: changed logger module parameters (dev).

--- src/LoggingDecorator/factory/logger.py
+++ src/LoggingDecorator/factory/logger.py
@@ -4,14 +4,18 @@ Module interface containing the types of Loggers.
 import logging
 from .custom_logger import CustomLogger
 from ..utils.shared_parameters import InnerParameters as ip
-# TODO: Wrap this class arround a decorator, so it will be easy to create new Logger types.
+#  Wrap this class arround a decorator, so it will be easy to create new Logger types.
 
 class LoggerStream(CustomLogger):
     """
     Class to build Log Streams to console (concrete product).
     """
     def __init__(self, wrapped):
-        super().__init__(self.logger_handler, logging, wrapped)
+        self.inner_parameters = ip()
+        self.wrapped = wrapped
+        self.logging = logging
+        self.logger_handler = self.logging
+        CustomLogger.__init__(self, self.logger_handler, logging, wrapped)
 
     @property
     def logger_handler(self):
@@ -20,13 +24,41 @@ class LoggerStream(CustomLogger):
         """
         return self._logger_handler
 
+    @property
+    def inner_parameters(self):
+        """
+        inner_parameters getter.
+        """
+        return self._inner_parameters
+
+    @property
+    def wrapped(self):
+        """
+        wrapped getter.
+        """
+        return self._wrapped
+
     @logger_handler.setter
-    def logger_handler(self):
+    def logger_handler(self, logging_object):
         """
         logger_handler setter.
         """
-        logger_handler = logging.StreamHandler()
-        logger_handler.setLevel(ip.logging_level)
-        logger_handler.setFormatter(ip.logging_stream_format)
-        self._logger_handler = logger_handler
+        handler = logging_object.StreamHandler()
+        handler.setLevel(self.wrapped.logging_level)
+        formatter = logging_object.Formatter(self.inner_parameters.logging_stream_format)
+        handler.setFormatter(formatter)
+        self._logger_handler = handler
         return self._logger_handler
+
+    @inner_parameters.setter
+    def inner_parameters(self, inner_parameter):
+        self._inner_parameters = inner_parameter
+        return self._inner_parameters
+
+    @wrapped.setter
+    def wrapped(self, _wrapped):
+        """
+        wrapped setter.
+        """
+        self._wrapped = _wrapped
+        return self._wrapped
